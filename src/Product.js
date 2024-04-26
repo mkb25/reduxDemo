b0863ffdc107c9eb52b24cd62592be64f7c51b5c
mkb25 <iammohankrishnabhat@gmail.com>
2024-04-26 12:18:11 +0530

    Second commit

--- src/Product.js
+++ src/Product.js
@@ -5,25 +5,29 @@ import { useDispatch } from "react-redux";
 import { addProd } from "./Redux/actions/prodactions";
 export default function Product() {
   const products = useSelector((state) => state.prodReducer.products);
-  let {detail} = useParams();
+  let { detail } = useParams();
   // console.log(addProd);
   const dispatch = useDispatch();
   return (
     <>
-      {
-        products.map((card, index) => {
-          if(card.title === detail){
-            return(
-              <div key={index}>
-                <h1>{card.title}</h1>
-                <p>{card.description}</p>
-                <img src={require(`../src/${card.img}`)} alt={card.title}/>
-                <button onClick={()=>{dispatch(addProd(card))}}>ADD to cart</button>
-              </div>
-            )
-          }
-        })
-      }
+      {products.map((card, index) => {
+        if (card.title === detail) {
+          return (
+            <div key={index}>
+              <h1>{card.title}</h1>
+              <p>{card.description}</p>
+              <img src={require(`../src/${card.img}`)} alt={card.title} />
+              <button
+                onClick={() => {
+                  dispatch(addProd(card));
+                }}
+              >
+                ADD to cart
+              </button>
+            </div>
+          );
+        }
+      })}
     </>
   );
 }
