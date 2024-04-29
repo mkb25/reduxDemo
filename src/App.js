ae37bb15c902efdf6d17681330c4f5e705b6e20a
Mkb25 <iammohankrishnabhat@gmail.com>
2024-04-29 11:47:45 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import Navbar1 from "./Navbar";
 import Catalog from "./Catalog";
 import "./App.css";
-import { useState } from "react";
+// import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   // const [count, setCount] = useState(0);
   return (
     <>
+      <BrowserRouter>
       <Navbar1 />
+      </BrowserRouter>
       {/* <h1>hello world</h1>
     <h2>Count: {count}</h2>
     <button onClick={()=>{setCount(count+1)}} class='btn'>Add {count}</button> */}
--- src/Cart.js
+++ src/Cart.js
@@ -1,28 +1,64 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from "react-redux";
-
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import { removeProd } from './Redux/actions/prodactions';
+import { useDispatch } from "react-redux";
+import Table from 'react-bootstrap/Table';
+import '../src/CSS/cart.css'
 export default function Cart() {
+  const dispatch = useDispatch();
     const products = useSelector((state) => state.prodReducer.cart);
+    const quant = useSelector((state) => state.prodReducer.quant);
+    const sum = useSelector((state) => state.prodReducer.sum);
+    const product ={
+      padding: '10px',
+    }
+    const container = {
+      padding: '7%',
+    }
     console.log(products);
   if(products.length === 0){
     return(
       <h1>Cart is Empty</h1>
     )
   } 
-//   else {
-//     return (
-//       <>
-//         <h1>Cart</h1>
-//         {
-//           products.map((card, index) => (
-//             <div key={index}>
-//               <h1>{card.title}</h1>
-//               <p>{card.description}</p>
-//               <img src={require(`../src/${card.img}`)} alt={card.title}/>
-//             </div>
-//           ))
-//         }
-//       </>
-//     )
-//   }
+  else {
+    return (
+      <>
+      <div style={container}>
+      <Table responsive border >
+        <thead>
+          <tr>
+            <th className='title'>Title</th>
+            <th></th>
+           
+            <th>Price</th>
+        
+          </tr>
+        </thead>
+        <tbody>
+          {
+            products.map((card, index) => (
+              <tr key={index}>
+                <td ><img src={require(`../src/${card.img}`) } alt="" /></td>
+                 <td >{card.title}</td> 
+                <td >₹ {card.price}</td>
+                <td><button onClick={()=>{dispatch(removeProd(card.title))}}>X</button></td>
+              </tr>
+            ))
+          }
+          <tr>
+            <td></td>
+            <td >Total</td>
+  
+            <td>₹ {sum} <br />(total quantity) {quant}</td>
+          </tr>
+        </tbody>
+      </Table>
+      </div>
+      </>
+    )
+  }
 }
